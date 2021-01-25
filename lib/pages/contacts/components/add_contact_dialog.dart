05a7acfb8ccb8d253356c0931bea54d73e4ae463
dinhkhang92 <dinh.khang92@hotmail.de>
2021-01-25 01:30:00 +0100

    contacts cubit added

--- lib/pages/contacts/components/add_contact_dialog.dart
+++ lib/pages/contacts/components/add_contact_dialog.dart
@@ -3,10 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tradedex/Global/GlobalConstants.dart';
 import 'package:tradedex/localization/app_localization.dart';
 import 'package:tradedex/pages/contacts/cubit/contacts_cubit.dart';
-import 'package:tradedex/pages/contacts/cubit/trading_code_cubit.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 
-import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
 
 class AddContactDialog extends StatefulWidget {
   @override
@@ -15,6 +14,7 @@ class AddContactDialog extends StatefulWidget {
 
 class _AddContactDialogState extends State<AddContactDialog> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  String inputId;
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +25,27 @@ class _AddContactDialogState extends State<AddContactDialog> {
         children: [
           Form(
             key: this._formKey,
-            child: TextFormField(
-              style: TextStyle(color: textColor),
-              validator: (id) => BlocProvider.of<TradingCodeCubit>(context).validateTradingCode(id),
-              onChanged: (id) => BlocProvider.of<ContactsCubit>(context).setIdInput(id),
-              autofocus: true,
-              cursorColor: buttonColor,
-              decoration: InputDecoration(
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: textColor),
-                ),
-                labelText: AppLocalizations.of(context).translate('PAGE_CONTACTS.DIALOG_ADD.TITLE'),
-                labelStyle: TextStyle(color: textColor),
-                hintText: '-LVePy20jxxxxxxxxxxx',
-                hintStyle: TextStyle(color: prefillTextColor),
+            child: Container(
+              width: MediaQuery.of(context).size.width - 166,
+              child: BlocBuilder<ContactsCubit, ContactsState>(
+                builder: (context, state) {
+                  return TextFormField(
+                    style: TextStyle(color: textColor),
+                    validator: (id) => _validateTradingCode(id, state),
+                    onChanged: (id) => this.inputId = id,
+                    autofocus: true,
+                    cursorColor: buttonColor,
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: textColor),
+                      ),
+                      labelText: AppLocalizations.of(context).translate('PAGE_CONTACTS.DIALOG_ADD.TITLE'),
+                      labelStyle: TextStyle(color: textColor),
+                      hintText: '-LVePy20jxxxxxxxxxxx',
+                      hintStyle: TextStyle(color: prefillTextColor),
+                    ),
+                  );
+                },
               ),
             ),
           ),
@@ -51,7 +58,7 @@ class _AddContactDialogState extends State<AddContactDialog> {
               ),
               color: iconColor,
               onPressed: () {
-                if (this._formKey.currentState.validate()) print("validate: true");
+                if (this._formKey.currentState.validate()) BlocProvider.of<ContactsCubit>(context).addContact(this.inputId);
                 // BlocProvider.of<ContactsCubit>(context).addContact();
                 // validateInputs();
                 // if (this.validId != null) {
@@ -65,5 +72,17 @@ class _AddContactDialogState extends State<AddContactDialog> {
         ],
       ),
     );
+  }
+
+  String _validateTradingCode(String id, ContactsState state) {
+    print(state.contacts.keys.contains(id));
+    if (id.length != 20)
+      return AppLocalizations.of(context).translate('PAGE_CONTACTS.INVALID_CODE');
+    else if (id[0] != '-')
+      return AppLocalizations.of(context).translate('PAGE_CONTACTS.INVALID_CODE');
+    else if (state.contacts.keys.contains(id))
+      return AppLocalizations.of(context).translate('PAGE_CONTACTS.INVALID_CODE_IN_LIST');
+    else
+      return null;
   }
 }
--- lib/pages/contacts/cubit/contacts_state.dart
+++ lib/pages/contacts/cubit/contacts_state.dart
@@ -1,12 +1,24 @@
 part of 'contacts_cubit.dart';
 
 @immutable
-abstract class ContactsState {}
+abstract class ContactsState {
+  final Map contacts;
+  ContactsState({this.contacts});
+}
 
-class ContactsInitial extends ContactsState {}
+class ContactsInitial extends ContactsState {
+  final Map contacts;
+  ContactsInitial({@required this.contacts});
+}
 
-class ContactsLoading extends ContactsState {}
+class ContactsLoading extends ContactsState {
+  final Map contacts;
+  ContactsLoading({@required this.contacts});
+}
 
-class ContactsLoaded extends ContactsState {}
+class ContactsLoaded extends ContactsState {
+  final Map contacts;
+  ContactsLoaded({@required this.contacts});
+}
 
 class ContactsError extends ContactsState {}
